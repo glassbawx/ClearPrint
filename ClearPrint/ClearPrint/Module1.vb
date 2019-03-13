bd43a65cc2d3793b16df6a5b7c7760f7d25d6187
glassbawx <miloeng@gmail.com>
2019-03-13 01:26:52 -0400

    Add files via upload

---
+++ ClearPrint/ClearPrint/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim service As ServiceProcess.ServiceController = New ServiceProcess.ServiceController("Print Spooler")
+        If ((service.Status.Equals(ServiceProcess.ServiceControllerStatus.Running)) Or
+                (service.Status.Equals(ServiceProcess.ServiceControllerStatus.Paused))) Then
+            service.Stop()
+            Kill("C:\Windows\System32\spool\PRINTERS\*.*")
+            service.Start()
+        Else
+            service.Start()
+
+        End If
+    End Sub
+End Module
